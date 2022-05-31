2c8dd2483d5b8b124882f672979385cd45b1d4c9
gerenciaitfvf <104863743+gerenciaitfvf@users.noreply.github.com>
2022-05-31 16:51:51 -0400

    update - first load to heroku

--- wp-config.php
+++ wp-config.php
@@ -20,16 +20,16 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'intranet' );
+define( 'DB_NAME', 'heroku_66a3069918af5f5' );
 
 /** Database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'b7ce434aad66de' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'root' );
+define( 'DB_PASSWORD', 'f8b08fc9' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'us-cdbr-east-05.cleardb.net' );
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
